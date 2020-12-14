6f0dd3ed30caa119ad1ddc0f9c3faf749750241c
phamdinhthien <phamdinhthien>
2020-12-14 23:20:50 +0700

    add demo

---
+++ Demo/src/components/button/MsButton.vue
@@ -0,0 +1,34 @@
+<template>
+    <button class="btn">
+        <div v-if="icon && !iconAfter" :class="icon" class="icon-left"></div>
+        <slot/>
+        <div v-if="icon && iconAfter" :class="icon"></div>
+    </button>
+</template>
+<script>
+export default {
+    props: {
+        icon: {
+            type: String
+        },
+        iconAfter: {
+            type: [String, Boolean],
+            default:  false
+        }
+    }
+}
+</script>
+<style scoped>
+.btn{
+    display: flex;
+    align-items: center;
+    background: #55468c;
+    border: #55468c;
+    border-radius: 4px;
+    padding: 4px 8px;
+    color: white;
+}
+.icon-left{
+    margin-right: 8px;
+}
+</style>
